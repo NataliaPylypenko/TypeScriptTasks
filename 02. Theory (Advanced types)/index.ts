9adfa715090667f60a5b603e1373112b69afd824
NataliaPylypenko <natalia28ivanivna@gmail.com>
2024-01-11 07:12:50 +0200

    Type Unknown

--- 02. Theory (Advanced types)/index.ts
+++ 02. Theory (Advanced types)/index.ts
@@ -200,3 +200,47 @@ type UserDic2 = {
 // };
 //
 // skills.forEach(skill => user.s.push);
+
+
+// ====================
+// Unknown
+
+let input: unknown;
+
+input = 3;
+input = ['as', 'df'];
+
+function run(i: unknown) {
+    if (typeof i ==='number') {
+        i++;
+    } else {
+        i
+    }
+}
+
+run(input);
+
+
+async function getData() {
+    try {
+        await fetch('');
+    } catch (error) {
+        if (error instanceof Error) {
+            console.log(error.message);
+        }
+    }
+}
+
+async function getDataForce() {
+    try {
+        await fetch('');
+    } catch (error) {
+        const e = error as Error;
+        console.log(e.message);
+    }
+}
+
+
+type U1 = unknown | number;
+
+type I1 = unknown & string;
