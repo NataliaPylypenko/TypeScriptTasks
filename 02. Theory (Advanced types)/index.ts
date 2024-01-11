b51f5e8b12055e82c67f77c2094e1b1a9888442a
NataliaPylypenko <natalia28ivanivna@gmail.com>
2024-01-11 07:12:14 +0200

    Type Void

--- 02. Theory (Advanced types)/index.ts
+++ 02. Theory (Advanced types)/index.ts
@@ -165,3 +165,38 @@ type UserDic2 = {
     [index: number]: User
 }
 
+// ====================
+// Void
+
+// function logId(id: string | number): void {
+//     console.log(id);
+// }
+//
+// const a = logId(1);
+//
+//
+// function multiply(f: number, s?: number) {
+//     if(!s) {
+//         return f * f;
+//     }
+// }
+//
+//
+// type voidFunc = () => void;
+//
+// const f1: voidFunc = () => {};
+//
+// const f2: voidFunc = () => {
+//     return true;
+// };
+//
+// const b = f2();
+//
+//
+// const skills = ['Dev', 'Devops'];
+//
+// const user = {
+//     s: []
+// };
+//
+// skills.forEach(skill => user.s.push);
